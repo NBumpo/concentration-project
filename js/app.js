bedf771f192e745fa855c420fa53615e92451fdf
nathanaelchojnacki <nbumpo11@gmail.com>
2023-10-06 11:10:09 -0500

    beginning outline

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// What will happen in the game?
+// 
+//10 cards laid out in front of player
+//5 suites of matches
+//Cards are randomized
+//30 second timer starts
+//4 wrong guesses means game end
+//Timer reaching 0 means game end
+//Reset Button goes back to initialized state
+//When game end element appear at top of screen stating win or loss
+//status with score 
+
+//------ CONSTANT VARIABLES
+
+
+
+
+
+
+
+//------- STATE VARIABLES
+
+
+
+
+
+//------- EVENT LISTENERS
+
+
+
+
+
+
+
+//-------- FUNCTIONS
+
+//init
+
+
+
+
+
+// render
