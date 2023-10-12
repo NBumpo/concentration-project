df1ba20bc871b343c7f14161a966ba7c4ede3ddc
nathanaelchojnacki <nbumpo11@gmail.com>
2023-10-12 11:34:29 -0500

    readme started

--- js/app.js
+++ js/app.js
@@ -1,216 +1,13 @@
 // What will happen in the game?
 // 
-//10 cards laid out in front of player
-//5 suites of matches
+//12 cards laid out in front of player
+//6 pairs of matches
 //Cards are randomized
-//30 second timer starts
 //4 wrong guesses means game end
-//Timer reaching 0 means game end
 //Reset Button goes back to initialized state
-//When game end element appear at top of screen stating win or loss
-//status with score 
+//When game end element appear at bottom of screen stating win or loss
 
-//------ CONSTANT VARIABLES
 
-//array of symbols to choose from
-//cached elements: buttons, wrong guess images, timer clock
-
-
-
-
-
-//------- STATE VARIABLES
-
-//matches made
-// wrong guesses
-
-
-
-//------- EVENT LISTENERS
-
-//button listeners
-
-
-//-------- FUNCTIONS
-
-//init 
-// randomize symbol cards
-// set wrong guesses to zero
-// start timer function
-
-//reveal cards function
-//select two cards at a time if cards are not the same they are 
-//obscured once again and +1 is added to the wrong guess variable
-
-
-//matches made function
-//if two cards of the same symbol are selected they stay revealed 
-
-
-
-// render
-//matches made
-//wrong guess visulization
-
-//---------------------------------------------------------------------
-//==============CONSTANTS================
-
-
-//const symbols = ['@', '#', '$', '%', '&', '@', '#', '$', '%', '&'];
-// const cardBtn = document.querySelector(".card");
-// const resetBtn = document.querySelector(".reset")
-
-// //==============STATE==============
-// const wrongGuesses = 0;
-// const matchesMade = 0;
-// const selectVar = 0;
-
-
-// //==========EVENT LISTENERS=========
-
-
-// cardBtn.addEventListener("click", set);
-// resetBtn.addEventListener("click", init);
-
-
-
-
-
-//===========FUNCTIONS==========
-//function shuffle(){
-
-//}
-// function init(){
-//     document.querySelector('main').innerHTML=''
-//     for (let i = 0; i < symbols.length; i++) {
-//         const crtBtn = document.createElement('button');
-//         crtBtn.textContent = symbols[i];
-//         document.querySelector('main').appendChild(crtBtn);
-//         crtBtn.addEventListener('click', selectFunc)
-//          shuffleArray()
-
-//        function render(){
-
-//         }
-// }
-// }
-// init ()
-
-// function set() {
-//     cardBtn.textContent = (symbols[(Math.floor(Math.random() * symbols.length))])
-// }
-
-
-// function shuffleArray() {
-//     let len = symbols.length,
-//         currentIndex;
-//     for (currentIndex = len - 1; currentIndex > 0; currentIndex--) {
-//         let randIndex = Math.floor(Math.random() * (currentIndex + 1) );
-//         const temp = symbols[currentIndex];
-//         symbols[currentIndex] = symbols[randIndex];
-//         symbols[randIndex] = temp;
-//     }
-// }
-
-// function selectFunc(e) {
-//     e.target.
-// }
-
-
-// const varMatchOne = document.getElementsByClassName('matchOne')
-// const varMatchTwo = document.querySelectorAll('div.matchTwo')
-// const varMatchThree = document.querySelectorAll('div.matchThree')
-// const varMatchFour = document.querySelectorAll('div.matchFour')
-// const varMatchFive = document.querySelectorAll('div.matchFive')
-
-
-// const resetBtn = document.querySelector(".reset");
-// resetBtn.addEventListener("click", init);
-
-
- 
-
-
-// function init(){
-//     let varSelection = 0;
-//     let arrSelection = [];
-// }
-
-// init()
-
-
-
-// CLASSES ===================
-//let varSelection = 0;
-//let arrSelection = [];
-
-
-
-
-
-
-
-// // ONE ===================
-// document.querySelector('.matchOneA').addEventListener('click', (e) => {
-//     e.target.classList.add('selectedMatchOne');
-//     e.target.classList.add('clicked');
-//     (varSelection += 1);
-//     arrSelection.push('blue');
-//    selectionCheck();
-//     return
-// }
-// )
-// document.querySelector('.matchOneB').addEventListener('click', (e) => {
-//     e.target.classList.add('selectedMatchOne',);
-//     e.target.classList.add('clicked');
-//     (varSelection += 1);
-//     arrSelection.push('blue');
-//     selectionCheck();
-//     return
-// }
-// )
-
-
-// function selectionCheck(){
-//     if ((varSelection >= 2) && (arrSelection[0] === arrSelection[1])){
-//         varSelection -= 2;
-//         while(arrSelection.length) {
-//             let pop = arrSelection.pop()
-//             console.log(pop)
-//         }
-//     const clickedList = document.getElementsByClassName('clicked');
-//         console.log(clickedList);
-
-//       (clickedList[0]).style.backgroundColor = ('green');
-//       (clickedList[1]).style.backgroundColor = ('green');
-//       (clickedList[0].classList.remove('clicked'));
-//       (clickedList[1].classList.remove('clicked'));
-//     } else if (varSelection >= 2) {
-//         document.querySelector('.matchOneA').classList.remove('selectedMatchOne');
-//     document.querySelector('.matchOneB').classList.remove('selectedMatchOne');
-//     document.querySelector('.matchTwoA').classList.remove('selectedMatchTwo');
-//     document.querySelector('.matchTwoB').classList.remove('selectedMatchTwo');
-//     document.querySelector('.matchThreeA').classList.remove('selectedMatchThree');
-//     document.querySelector('.matchThreeB').classList.remove('selectedMatchThree');
-//     document.querySelector('.matchFourA').classList.remove('selectedMatchFour');
-//     document.querySelector('.matchFourB').classList.remove('selectedMatchFour');
-//     document.querySelector('.matchFiveA').classList.remove('selectedMatchFive');
-//     document.querySelector('.matchFiveB').classList.remove('selectedMatchFive');
-//         varSelection -= 2;
-//         while(arrSelection.length) {
-//             let pop = arrSelection.pop()
-//             console.log(pop)
-//         }
-    
-   
-//     console.log('if is running')
-
-// }
-// }
-
-//e.target.className
-
-//Assigning classes from an array to the div elements randomly
 
 //reset button
 const resetBtn = document.querySelector('.reset')
@@ -218,7 +15,7 @@ resetBtn.addEventListener('click',init)
 
 
 let wrongGuesses = 0;
-const allDiv = document.querySelectorAll('div')
+const allDiv = document.querySelectorAll('main div')
 const pairs = ['A','A','B','B','C','C','D','D','E','E','F','F']
 let clickedCards = [];
 let arrCorrectMatches = [];
@@ -276,10 +73,11 @@ function shuffle(array) {
 function init() {
     if (document.querySelector('main').contains(winP)){
 
-        document.querySelector('main').removeChild(winP)
+            document.querySelector('main').removeChild(winP)
 
     } else if (document.querySelector('main').contains(loseP)) {
-        document.querySelector('main').removeChild(loseP)
+            
+            document.querySelector('main').removeChild(loseP)
     }
     arrCorrectMatches = [];
     clickedCards = [];
@@ -307,11 +105,9 @@ function init() {
 
 // SELECTION CHECK FUNCTION ==========================
 function selectionCheck() {
-console.log(clickedCards[0].className)
-console.log(clickedCards[1].className)
+
 if (clickedCards[0].className === clickedCards[1].className){
-    console.log(clickedCards)
-    console.log(clickedCards[1])
+   
         clickedCards[1].classList.add('correctmatch')
         clickedCards[0].classList.add('correctmatch')
         arrCorrectMatches.push(clickedCards[1].classList)
@@ -325,11 +121,16 @@ else if (clickedCards[0].className !== clickedCards[1].className) {
     (clickedCards[0].classList.add('hidden'));
 wrongGuesses += 1;
 clickedCards = [];
-console.log(wrongGuesses)
+// if (wrongGuesses >= 1){
+//     document.querySelector('.life1').classList.remove('')
+// }
+
 winCheck()
 } 
 else return;
 
+
+// functions for appending the results status
 }
 function appendWinP() {
 
@@ -339,14 +140,16 @@ function appendLoseP() {
 
     document.querySelector('main').appendChild(loseP)
 }
+
+//function for win/lose condition
 function winCheck() {
    
     if (arrCorrectMatches.length === 12) {
-       // winP.classList.add('resultsmessage')
+     
         appendWinP()
     } 
     if (wrongGuesses >= 5) {
-       // loseP.classList.add('resultsmessage')
+      
         appendLoseP()
     }
 }
---
+++ readme.md
@@ -0,0 +1,37 @@
+# __Concentration__
+
+ In Concentration the goal is to match up the pairs of colors until all tiles are green.
+
+
+
+
+
+
+
+## Screenshots
+
+![Initialized](https://imgur.com/TZpPSrI)  
+![Selecting Tiles](https://imgur.com/xGsZnNc)  
+![Lose State](https://imgur.com/Fzw66GT)  
+![Win State](https://imgur.com/stIyGQf)
+
+
+## Technologies Used
+HTML, CSS, and JavaScript were used to build this project.
+
+
+
+## Getting Started
+    1. You can select up to 2 tiles at a time.
+    2. There are 6 pairs of colors.
+    3. 5 wrong guesses is a game over.
+    4. Match all the tiles to win.
+    5. Press reset to shuffle the tiles and start over.
+
+
+
+## Potential Future Enhancements
+    1. Wrong guesses visual
+    2. Timer countdown
+    3. Scoring
+    
